e73f1ee57fd310be559493a813e1f86a5fb423ae
Amit-Biderman <amit.biderman@risecodes.com>
2022-03-03 18:47:38 +0200

    feature - fixed set output

--- pkgs/utils/utils.go
+++ pkgs/utils/utils.go
@@ -15,15 +15,17 @@ func GetEnv(key string) string {
 }
 
 func SetTagOutputName(value string){
+	// Set repo tag
+	repo_tag := value
+	log.Println("Setting repo tag as:", repo_tag)
+	log.Printf("Running: ::set-output name=repo_tag::%s`", repo_tag)
+	fmt.Printf(`::set-output name=repo_tag::%s`, repo_tag)
+	
 	// Set ecr tag
 	ecr_tag := version.RemovePrefix(value, "v")
 	log.Println("Setting ecr tag as:", ecr_tag)
 	log.Printf("Running: ::set-output name=ecr_tag::%s`", ecr_tag)
 	fmt.Printf(`::set-output name=ecr_tag::%s`, ecr_tag)
 
-	// Set repo tag
-	repo_tag := value
-	log.Println("Setting repo tag as:", repo_tag)
-	log.Printf("Running: ::set-output name=repo_tag::%s`", repo_tag)
-	fmt.Printf(`::set-output name=repo_tag::%s`, repo_tag)
+
 }
