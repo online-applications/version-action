6f0163838db3e68d0572f14512f693173c8f9213
Vladimir Faile <vladimir.faile@risecodes.com>
2022-12-05 14:08:23 +0200

    DEVOPS-184/Github action - Fix set-output deprecation
    [bugfix]
    The `set-output` command is deprecated

--- pkgs/utils/utils.go
+++ pkgs/utils/utils.go
@@ -1,9 +1,9 @@
 package utils
 
 import (
-	"fmt"
 	"log"
 	"os"
+
 	"version-action/pkgs/version"
 )
 
@@ -15,16 +15,29 @@ func GetEnv(key string) string {
 }
 
 func SetTagOutputName(value string) {
+
+	// Open GITHUB output file
+	ghOutputFile, err := os.OpenFile(os.Getenv("GITHUB_OUTPUT"), os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
 	// Set ecr tag
 	ecrTag := version.RemovePrefix(value, "v")
-	log.Println("Setting ecr tag as:", ecrTag)
-	fmt.Printf(`::set-output name=ecr_tag::%s`, ecrTag)
-	fmt.Print("\n")
+	log.Println("Setting ecr tag as: ", ecrTag)
+	ecrTagOutput := "ecr_tag=" + ecrTag + "\n"
+	_, err = ghOutputFile.WriteString(ecrTagOutput)
+	if err != nil {
+		panic(err)
+	}
+
 	// Set repo tag
-	repoTag := value
-	log.Println("Setting repo tag as:", repoTag)
-	fmt.Printf(`::set-output name=repo_tag::%s`, repoTag)
-	fmt.Print("\n")
+	log.Println("Setting repo tag as: ", value)
+	repoTagOutput := "repo_tag=" + value + "\n"
+	_, err = ghOutputFile.WriteString(repoTagOutput)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func SliceContains(s []string, str string) bool {
